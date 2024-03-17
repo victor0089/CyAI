51f70dba675635d73001eb48cea8c0bbd33a422a
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-03-17 14:11:12 +0200

    Create data_processing.pyx

---
+++ cyai/utils/data_processing.pyx
@@ -0,0 +1,9 @@
+# data_processing.pyx
+cimport numpy as np
+import numpy as np
+
+cpdef void preprocess_data(np.ndarray[np.float64_t, ndim=2] data):
+    # Implement data preprocessing functions using Cython
+    pass
+
+# Add other data processing functions if needed
