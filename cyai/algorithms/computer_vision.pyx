05b6491fc8de3df8c6193e3ec3a0a438851074fe
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-03-17 14:10:11 +0200

    Create computer_vision.pyx

---
+++ cyai/algorithms/computer_vision.pyx
@@ -0,0 +1,13 @@
+# computer_vision.pyx
+cimport numpy as np
+import numpy as np
+
+cpdef void object_detection(np.ndarray[np.float64_t, ndim=3] images):
+    # Implement Object Detection using Cython
+    pass
+
+cpdef void image_classification(np.ndarray[np.float64_t, ndim=3] images, np.ndarray[np.float64_t, ndim=1] labels):
+    # Implement Image Classification using Cython
+    pass
+
+# Add implementations for other computer vision algorithms
